828e14594c322ebacf6c1fd5ef0f5dfe4e3e1b6a
DevArthuro <caorrego38@soy.sena.edu.co>
2024-06-17 18:33:55 -0500

    feat: finish todo practice cache storage with RTX

---
+++ 6. to-do-RTX-Query-react-vite/src/pages/todoList.jsx
@@ -0,0 +1,13 @@
+import FormTodo from "../components/formTodo.component";
+import TodoList from "../components/todoList.component";
+
+const TodoListPage = () => {
+  return (
+    <div>
+      <FormTodo />
+      <TodoList />
+    </div>
+  );
+};
+
+export default TodoListPage;
